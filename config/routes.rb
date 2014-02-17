3c5bb077f6193dc04982e4fa3ef92ff8b9932c04
daddy-warbucks <warbucks@klassifieds.com.au>
2014-02-17 12:16:51 +1100

    Added pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Klassifieds::Application.routes.draw do
+  get "pages/home"
+  get "pages/about"
+  get "pages/contact"
+  get "pages/help"
+  get "pages/terms"
+  get "pages/privacy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
