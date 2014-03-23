e8a8e63a31ef92e679795db2052a78780bcda6b3
daddy-warbucks <warbucks@klassifieds.com.au>
2014-03-23 15:06:11 +1100

    image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,5 @@
 class Image < ActiveRecord::Base
   belongs_to :item
   has_attached_file :pic, :styles => { :large => "800x800", :medium => "400x400>", :small => "200x200>" }
-  validates_attachment content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/gif', 'image/png']}, size: { less_than: 4.megabytes}
 
 end
